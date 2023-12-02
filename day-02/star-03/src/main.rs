b03350522fd6b77c9751da701d82c73562807907
Nathaniel Bennett <me@nathanielbennett.com>
2023-12-02 11:56:19 -0500

    Day 2

---
+++ day-02/star-03/src/main.rs
@@ -0,0 +1,65 @@
+use std::fs;
+
+use peggle::{Parse, ParseError};
+use peggle_derive::Parse;
+
+
+#[derive(Parse)]
+#[peg("Game <game_number>: (<handfuls>; )*<handfuls>")]
+struct GameRound {
+    game_number: usize,
+    handfuls: Vec<Handful>,
+}
+
+// Annoyed that this bit is rather verbose...
+#[derive(Parse)]
+#[peg("(<r>, <g>, <b>)|(<r>, <b>, <g>)|(<b>, <r>, <g>)|(<b>, <g>, <r>)|(<g>, <r>, <b>)|(<g>, <b>, <r>)|(<r>, <b>)|(<r>, <g>)|(<b>, <r>)|(<b>, <g>)|(<g>, <r>)|(<g>, <b>)|<r>|<b>|<g>")]
+struct Handful {
+    r: Option<Red>,
+    g: Option<Green>,
+    b: Option<Blue>,
+}
+
+#[derive(Parse)]
+#[peg("<0> red")]
+struct Red(usize);
+
+#[derive(Parse)]
+#[peg("<0> green")]
+struct Green(usize);
+
+#[derive(Parse)]
+#[peg("<0> blue")]
+struct Blue(usize);
+
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let mut total_correct = 0;
+
+    for (idx, line) in input.lines().enumerate() {
+        let round = GameRound::parse(line).expect("invalid input detected: {}");
+        if is_possible_game(&round) {
+            total_correct += round.game_number;
+        }
+    }
+
+    println!("{}", total_correct);
+}
+
+fn is_possible_game(round: &GameRound) -> bool {
+    for handful in &round.handfuls {
+        if let Some(Red(13..)) = handful.r {
+            return false
+        }
+        if let Some(Green(14..)) = handful.g {
+            return false
+        }
+        if let Some(Blue(15..)) = handful.b {
+            return false
+        }
+    }
+    
+    true
+}
---
+++ day-02/star-04/src/main.rs
@@ -0,0 +1,69 @@
+use std::{cmp, fs};
+
+use peggle::{Parse, ParseError};
+use peggle_derive::Parse;
+
+
+#[derive(Parse)]
+#[peg("Game <game_number>: (<handfuls>; )*<handfuls>")]
+struct GameRound {
+    game_number: usize,
+    handfuls: Vec<Handful>,
+}
+
+// Annoyed that this bit is rather verbose...
+#[derive(Parse)]
+#[peg("(<r>, <g>, <b>)|(<r>, <b>, <g>)|(<b>, <r>, <g>)|(<b>, <g>, <r>)|(<g>, <r>, <b>)|(<g>, <b>, <r>)|(<r>, <b>)|(<r>, <g>)|(<b>, <r>)|(<b>, <g>)|(<g>, <r>)|(<g>, <b>)|<r>|<b>|<g>")]
+struct Handful {
+    r: Option<Red>,
+    g: Option<Green>,
+    b: Option<Blue>,
+}
+
+#[derive(Parse)]
+#[peg("<0> red")]
+struct Red(usize);
+
+#[derive(Parse)]
+#[peg("<0> green")]
+struct Green(usize);
+
+#[derive(Parse)]
+#[peg("<0> blue")]
+struct Blue(usize);
+
+
+fn main() {
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let mut sum_of_powers = 0usize;
+
+    for (idx, line) in input.lines().enumerate() {
+        let round = GameRound::parse(line).expect("invalid input detected: {}");
+
+        let (min_red, min_green, min_blue) = min_cubes(&round);
+        sum_of_powers += min_red * min_green * min_blue;
+    }
+
+    println!("{}", sum_of_powers);
+}
+
+fn min_cubes(round: &GameRound) -> (usize, usize, usize) {
+    let mut min_red = 0;
+    let mut min_green = 0;
+    let mut min_blue = 0;
+
+    for handful in &round.handfuls {
+        if let Some(Red(curr_red)) = handful.r {
+            min_red = cmp::max(min_red, curr_red);
+        }
+        if let Some(Green(curr_green)) = handful.g {
+            min_green = cmp::max(min_green, curr_green);
+        }
+        if let Some(Blue(curr_blue)) = handful.b {
+            min_blue = cmp::max(min_blue, curr_blue);
+        }
+    }
+
+    (min_red, min_green, min_blue)   
+}
